105340a2c7ca8761f4d986792c1c04a62285fcf1
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-22 08:12:08 +0000

    conversion of datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("achint")
+/*let score = false
+let convertInNumb = Number(score)
+console.log(typeof score)
+console.log(typeof(score))
+console.log(typeof(convertInNumb))
+console.log(convertInNumb)
+*/
+let loggedIn = "achint"
+let BooleanLoggedIn = Boolean(loggedIn)
+console.log(typeof BooleanLoggedIn)
+console.log(BooleanLoggedIn)
