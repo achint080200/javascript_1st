a8af43f96c085900e1c9878b2576f4223b725722
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-22 12:33:58 +0000

    conversion operation

---
+++ 01_basics/conersion_operation.js
@@ -0,0 +1,12 @@
+/*let numberOne = 1
+let numberTwo = -numberOne
+console.log(numberTwo)
+console.log(1 + "1")
+console.log("1"+"1"+1)
+console.log(1+1+1+"1")
+*/
+//let hii={1:"hello",2:3}
+//console.log(typeof hii)
+let y=6
+console.log(y++)
+console.log(++y)
