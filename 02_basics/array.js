f3bc30a0893faa5abbba2cf5e1de144c753ef9b3
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-30 07:37:31 +0000

    array basics

---
+++ 02_basics/array.js
@@ -0,0 +1,20 @@
+const myarr = new Array("hello",1,2,3,"kaisa")
+//console.log(myarr);
+/*myarr.push(4)
+myarr.push(5)
+myarr.pop()
+myarr.pop()
+myarr.unshift("first")
+myarr.shift()
+console.log(myarr);
+
+console.log(myarr.includes(1))
+console.log(myarr.indexOf("kaisa"));
+const newarr = myarr.join()
+console.log( typeof newarr);
+*/
+let firstArr = new Array(1,2,3,4,5,6)
+console.log(firstArr.slice(0,3));
+console.log("a", firstArr)
+console.log(firstArr.splice(0,3));
+console.log("b",firstArr)
