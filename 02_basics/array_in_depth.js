e8f25788859bd721e2bc3f09c4b9f53dfa3464db
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-30 08:07:55 +0000

    array in depth

---
+++ 02_basics/array_in_depth.js
@@ -0,0 +1,16 @@
+let fstArr = [1,2,3]
+let sndArr = [3,4,5]
+//fstArr.push(sndArr)
+//console.log(fstArr);
+//let newArr = fstArr.concat(sndArr)
+//console.log(newArr);
+//let NewArr = [...fstArr,...sndArr]
+//console.log(NewArr);
+
+//const uniqArray = [1,2,3,[1,2,3],[1,2,[1,2,3]]]
+//const oneArray = uniqArray.flat(Infinity)
+//console.log(oneArray);
+
+//console.log(Array.from("achint"))
+let score1=100,score2 = 200,score3 = 300
+console.log(Array.of(score1,score2,score3));
