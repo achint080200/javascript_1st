4cf705268aa7dfbe777c88f02194e25a6d1c1831
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-30 19:56:37 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,36 @@
+// function hello(){
+//     console.log("hello user")
+// }
+// console.log(hello)
+// function addTwoNum(number1,number2){
+//     console.log(number1+number2)
+// }
+// let result=addTwoNum(10,20)
+// console.log(result);
+// function userLogged(username){
+//     if(username === undefined){
+//         console.log("enter a name")
+//         return
+//     }
+//     console.log(`${username} logged in`)
+
+// }
+// userLogged("achint")
+// function calculatecartprice(...num1){
+//     return num1
+// }
+// console.log(calculatecartprice(1,2,3,4,5))
+
+// let user={
+//     username : "achint",
+//     prices:100
+// }
+// function makeadeal(anyobject){
+//     console.log(`username is ${anyobject.username} and the price is ${anyobject.price}`)
+// }
+// makeadeal(user)
+let arra = [1,2,3]
+function mkarray(newarray){
+    return newarray[0]
+}
+console.log(mkarray(arra))
