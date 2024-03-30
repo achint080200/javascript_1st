5845c0c9e6d001cc249007eaa8a149183dd95dba
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-30 10:14:09 +0000

    objecct

---
+++ 02_basics/01_object.js
@@ -0,0 +1,17 @@
+let cityname = Symbol()
+let obj1 = {
+    name : "achint",
+    [cityname]:"vadodara",
+    emailId:"achint@gmail.com",
+    "full name": "parmar"
+
+}
+//console.log(obj1.name);
+//console.log(obj1["full name"]);
+//console.log(obj1[cityname]);
+//obj1.emailId = "achint0802@gmail.com"
+//console.log(obj1);
+// Object.freeze(obj1)
+// obj1.name="hahah"
+// console.log(obj1);
+
