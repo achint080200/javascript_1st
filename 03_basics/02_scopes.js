cdbbdbdc3ce51be6f052a7b4ee93269dc8a7126e
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-03-31 06:11:48 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// function f1(){
+//     let x=10
+// }
+// console.log(x);
+function one(){
+    let usrname ="achint"
+    function two(){
+        let website = "youtube"
+        console.log(usrname)
+    }
+    two()
+}
+one()
