bb92aec858dfd6706472530bbde2a70163105785
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-04-01 20:34:18 +0000

    for loop

---
+++ 05_loops/01_for.js
@@ -0,0 +1,15 @@
+// let array=[10,20,30,40,50,60]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+for (let index = 1; index < 11; index++) {
+   
+    console.log(`we gonna take ${index}`);
+    for (let i = 1; i< 11; i++) {
+        console.log(`${index} * ${i}:`,index*i);;
+        
+    }
+    
+}
