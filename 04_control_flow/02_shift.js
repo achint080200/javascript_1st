52c6c2b2ca8f87b1a57f3c496e44f32f6c06f62e
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-04-01 15:39:16 +0000

    shift

---
+++ 04_control_flow/02_shift.js
@@ -0,0 +1,54 @@
+let month = "march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("febuary");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        //break;
+    case 4:
+        console.log("april");
+        
+        break;
+    case 5:
+        console.log("may");
+        
+        break;
+    case 6:
+        console.log("june");
+        
+        break;
+    case 7:
+        console.log("july");
+        
+        break;
+    case 8:
+        console.log("august");
+        
+        break;
+    case 9:
+        console.log("september");
+        
+        break;
+    case 10:
+        console.log("october");
+        
+        break;
+    case 11:
+        console.log("november");
+        
+        break;
+    case 12:
+        console.log("december");
+        
+        break;
+
+    default:
+        break;
+}
