ecf1053ece1a0e6213de76f89379fc2c97e6c1fe
achint080200 <164375170+achint080200@users.noreply.github.com>
2024-04-01 17:40:39 +0000

    truthy

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,24 @@
+// let username = "achint"
+// if (username) {
+//     console.log("got the user ");
+// } else {
+//     console.log("did not get the user");
+// }
+
+// falsy values 
+// false,0,null,undefined,"",BigInt 0n,NaN
+
+// truthy values
+// [],{},true," ","false",
+// let obj = {
+//     name:"achint",
+//     city:"vadodara",
+
+// }
+// console.log(Object.keys(obj).length)
+
+// ternitary operator
+// condition ? true : false
+
+let helloUser = "achint"
+helloUser ? console.log("user logged in"):console.log("no one logged in");
